9c1fa5db6b417c1306094a47cbe75183afd46ee5
Rodrigo Q. Saramago <r0qs@fr4c74l.com>
2014-03-15 17:39:05 -0300

    Prepare to build codec

--- libavcodec/rpih264.c
+++ libavcodec/rpih264.c
@@ -19,6 +19,11 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include "libavutil/opt.h"
+#include "avcodec.h"
+#include "internal.h"
+#include "bcm_host.h"
+
 typedef struct OMXContext {
 } OMXContext;
 
